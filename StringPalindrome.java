48bd43e5a30626d4f62312165f03678c140deb57
Gouse.C <140190450+Gouse-c@users.noreply.github.com>
2024-10-19 22:16:19 +0530

    Add files via upload

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -3,17 +3,17 @@ class StringPalindrome
 {
 	public static void main(String args[])
 	{
-		Scanner sc = new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the String : ");
 		String str=sc.nextLine();
 		String rev=new StringBuffer(str).reverse().toString();
 		if(str.equals(rev))
 		{
-			System.out.println("It is String Palindrome...");
+			System.out.println("Palindrome String..");
 		}
 		else
 		{
-			System.out.println("Not a String Palindrome...");
+			System.out.println("Not a Palindrome String..");
 		}
 	}
 }

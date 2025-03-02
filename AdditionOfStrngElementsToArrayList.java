b0789ef6a681ce378de8f57cf3e8b49a6a0b7636
Gouse.C <140190450+Gouse-c@users.noreply.github.com>
2025-03-02 19:43:23 +0530

    Add files via upload

---
+++ AdditionOfStrngElementsToArrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+class AdditionOfStrngElementsToArrayList{
+          public static void main(String args[]){
+                    ArrayList<String> fruits = new ArrayList<String>();
+                    fruits.add("Apple");
+                    fruits.add("Banana");
+                    fruits.add("Custard Apple");
+                    fruits.add("Dragon Fruit");
+                    fruits.add("EldBerry");
+                    fruits.add("Fig");
+                    fruits.add("Guava");
+                    System.out.println(" "+ fruits);
+          }
+}

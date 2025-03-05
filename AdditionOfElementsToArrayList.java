fdb31d93df5d71e147d049e7d27bbb4eac013de1
Gouse.C <140190450+Gouse-c@users.noreply.github.com>
2025-03-05 23:44:03 +0530

    Add files via upload

---
+++ AdditionOfElementsToArrayList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class AdditionOfElementsToArrayList{
+	public static void main(String args[]){
+		ArrayList<Integer> a1 = new ArrayList<>();
+		a1.add(10);
+		a1.add(20);
+		a1.add(30);
+		a1.add(40);
+		a1.add(50);
+		System.out.println(" " + a1);
+	}
+}
